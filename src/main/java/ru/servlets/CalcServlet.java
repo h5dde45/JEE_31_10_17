1eaf592f8e0a94e91a63e6a2e0d9f699401cde3d
tmvf <tmvf@yandex.ru>
2017-11-29 20:27:02 +0300

    izm_3

--- src/main/java/ru/servlets/CalcServlet.java
+++ src/main/java/ru/servlets/CalcServlet.java
@@ -1,6 +1,7 @@
 package ru.servlets;
 
 import ru.calc.OperationType;
+import ru.calc.TestObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,13 +12,22 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "CalcServlet", urlPatterns = "/CalcServlet")
 public class CalcServlet extends HttpServlet {
-    private ArrayList<String> listOperation = new ArrayList<>();
+//    ArrayList<String> listOperation = new ArrayList<>();
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        Map<String, List> sessionMap = (Map<String, List>) request.getServletContext()
+                .getAttribute("sessionMap");
+
+        if (sessionMap == null) {
+            sessionMap = new LinkedHashMap<>();
+        }
 
         PrintWriter out = response.getWriter();
         out.println("<title>CalcServlet</title>");
@@ -28,11 +38,16 @@ public class CalcServlet extends HttpServlet {
             String operation = request.getParameter("operation");
 
             HttpSession session = request.getSession(true);
+            request.getServletContext().setAttribute("obj", new TestObject("NewName"));
+
+
             OperationType operationType = OperationType.valueOf(operation.toUpperCase());
             double result = calcResult(operationType, one, two);
+            ArrayList<String> listOperation;
 
             if (session.isNew()) {
-                listOperation.clear();
+                listOperation = new ArrayList<>();
+//                listOperation.clear();
             } else {
                 listOperation = (ArrayList<String>) session.getAttribute("formula");
             }
@@ -40,15 +55,34 @@ public class CalcServlet extends HttpServlet {
             listOperation.add(one + " " + operationType.getValue() + " "
                     + two + " = " + result);
             session.setAttribute("formula", listOperation);
+
+            out.println("<table cellpadding=\"20\">");
+            out.println("<tr>");
+            out.println("<td>");
+
             out.println("<h2>Id сессии: " + session.getId() + "</h2>");
-            out.println("<h2>Кол-во операций: " + listOperation.size() + "</h2>");
+//            out.println("<h2>Кол-во операций: " + listOperation.size() + "</h2>");
 
             for (String s : listOperation) {
                 out.println("<h3>" + s + "</h2>");
             }
-
+            out.println("</td>");
+            sessionMap.put(session.getId(), listOperation);
+            this.getServletContext().setAttribute("sessionMap", sessionMap);
+            out.println("<td>");
+            for (Map.Entry<String, List> entry : sessionMap.entrySet()) {
+                List listOper = entry.getValue();
+                out.println("<h2>Id сессии: " + entry.getKey() + "</h2>");
+                for (Object s : listOper) {
+                    out.println("<h3>" + s + "</h2>");
+                }
+            }
+            out.println("</td>");
+            out.println("</tr>");
+            out.println("</table>");
         } catch (Exception e) {
-            out.println("<h3 style=\"color:red\">Ошибка</h3>");
+//            out.println("<h3 style=\"color:red\">Ошибка</h3>");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         } finally {
             out.close();
         }
--- src/main/java/ru/servlets/CheckOperationServlet.java
+++ src/main/java/ru/servlets/CheckOperationServlet.java
@@ -1,11 +1,16 @@
 package ru.servlets;
 
+import ru.calc.TestObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 @WebServlet(name = "CheckOperationServlet", urlPatterns = "/CheckOperation")
 public class CheckOperationServlet extends HttpServlet {
@@ -13,6 +18,24 @@ public class CheckOperationServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
+        PrintWriter out = response.getWriter();
+        out.println("<title>CheckOperation</title>");
+
+        try {
+            HttpSession session = request.getSession(true);
+            ArrayList<String> listOperation = (ArrayList<String>) session.getAttribute("formula");
+            out.println("<h2>Id сессии: " + session.getId() + "</h2>");
+            for (String s : listOperation) {
+                out.println("<h3>" + s + "</h2>");
+            }
+            out.println("<h3>" + ((TestObject)request.getServletContext().
+                    getAttribute("obj")).getName() + "</h2>");
+
+        } catch (Exception e) {
+            out.println("<h3 style=\"color:red\">Ошибка</h3>");
+        } finally {
+            out.close();
+        }
     }
 
 
