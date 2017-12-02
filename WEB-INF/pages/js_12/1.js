da6e6e6b0f93349c7bb65e00dac579db6d25af4d
tmvf <tmvf@yandex.ru>
2017-12-02 14:24:27 +0300

    izm_9

--- WEB-INF/pages/js_12/1.js
+++ WEB-INF/pages/js_12/1.js
@@ -1,11 +1,28 @@
-document.getElementById('slider-left').onclick = sliderLeft;
-
+// document.getElementById('slider-left').onclick = sliderLeft;
+autoSlider();
 var left = 0;
-function sliderLeft() {
-    var polosa = document.getElementById("polosa");
-    left -= 128;
-    if (left < -512) {
-        left = 0;
-    }
-    polosa.style.left = left + 'px';
+var timer;
+
+function autoSlider() {
+    timer = setTimeout(function () {
+        var polosa = document.getElementById("polosa");
+        left -= 128;
+        if (left < -512) {
+            left = 0;
+            clearTimeout(timer);
+        }
+        polosa.style.left = left + 'px';
+        autoSlider();
+    }, 1000);
 }
+
+// function sliderLeft() {
+//     var polosa = document.getElementById("polosa");
+//     left -= 128;
+//     if (left < -512) {
+//         left = 0;
+//         clearTimeout(timer);
+//     }
+//     polosa.style.left = left + 'px';
+//     autoSlider();
+// }
