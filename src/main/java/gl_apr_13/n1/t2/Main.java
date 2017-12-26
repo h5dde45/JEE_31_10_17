172c2e580ed0cae322cb46e09937f97a23dc2464
tmvf <tmvf@yandex.ru>
2017-12-26 19:48:12 +0300

    izm_27

---
+++ src/main/java/gl_apr_13/n1/t2/Main.java
@@ -0,0 +1,10 @@
+package gl_apr_13.n1.t2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<Integer> integers=new ArrayList<>();
+    }
+}
